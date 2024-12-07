e4c1eeca391aef9b468125a36debe15b96194c1d
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-07 10:14:34 +0330

    Initial Commit

---
+++ UI4-WordScramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UI4-WordScramble
+//
+//  Created by Sothesom on 17/09/1403.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UI4-WordScramble/UI4_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI4_WordScrambleApp.swift
+//  UI4-WordScramble
+//
+//  Created by Sothesom on 17/09/1403.
+//
+
+import SwiftUI
+
+@main
+struct UI4_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
